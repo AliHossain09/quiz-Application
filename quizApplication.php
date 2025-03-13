9931432992db0f26d042b179fa80ba07e40398bd
AliHossain09 <ali.hoossainn@gmail.com>
2025-03-13 16:02:21 +0600

    Compare the user's correct answers

--- quizApplication.php
+++ quizApplication.php
@@ -1,5 +1,16 @@
 <?php 
+
+// Define function to evaluate the quiz
 function evaluateQuiz(array $questions, array $answers): int {
     $score = 0;
+
+    // Compare the user's correct answers
+    foreach ($questions as $index => $question) {
+        if (isset($answers[$index]) && strtolower(trim($answers[$index])) === strtolower(trim($question['correct']))) {
+            $score++;
+        }
+    }
+
+    return $score;
 }
 ?>
